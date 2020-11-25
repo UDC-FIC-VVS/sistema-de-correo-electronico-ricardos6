c2218667fc03048e845169e13945612893cccbb9
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-11-25 13:45:48 +0100

    tests unitarios manuales entidad Imagen

---
+++ src/test/java/gal/udc/fic/vvs/email/archivo/ImagenTest.java
@@ -0,0 +1,47 @@
+package gal.udc.fic.vvs.email.archivo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ImagenTest {
+	@Test
+	 public void testObtenerNombre() {
+		 
+		 String nombreImagen = "Nombre Imagen";
+		 String contenido = "Contenido Imagen";
+		 Imagen imagen = new Imagen(nombreImagen, contenido);
+		 
+		 assertEquals(nombreImagen, imagen.obtenerNombre());
+	 }
+	 
+	 @Test
+	 public void testObtenerContenido() {
+		 
+		 String nombreImagen = "Nombre Imagen";
+		 String contenidoImagen = "Contenido Imagen";
+		 Imagen imagen = new Imagen(nombreImagen, contenidoImagen);
+		 
+		 assertEquals(contenidoImagen, imagen.obtenerContenido());
+	 }
+	 
+	 @Test
+	 public void testObtenerTamaño() {
+		 
+		 String nombreImagen = "Nombre Imagen";
+		 String contenido = "Contenido Imagen";
+		 Imagen imagen = new Imagen(nombreImagen, contenido);
+		 
+		 assertEquals(contenido.length(), imagen.obtenerTamaño());
+	 }
+	 
+	 @Test
+	 public void testObtenerPreVisualizacion() {
+		 
+		 String nombreImagen = "Nombre Imagen";
+		 String contenido = "Contenido Imagen";
+		 Imagen imagen = new Imagen(nombreImagen, contenido);
+		 
+		 assertEquals(nombreImagen + "(" + imagen.obtenerTamaño() + " bytes, " +  imagen.obtenerMimeType() + ")", imagen.obtenerPreVisualizacion());
+	 }
+}
