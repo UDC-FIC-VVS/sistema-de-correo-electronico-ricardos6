8ec5bc7827d145d918883603ffa2157442b5431d
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-11-25 21:19:09 +0100

    Mejora cobertura de Entidad ArchivadorSimple

--- src/test/java/gal/udc/fic/vvs/email/archivador/ArchivadorSimpleTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/ArchivadorSimpleTest.java
@@ -16,9 +16,23 @@ public class ArchivadorSimpleTest {
 		String nombreArchivadorSimple = "Archivador Simple";
 		int espacio = 10;
 		
-		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
 		
 		assertEquals(nombreArchivadorSimple, archivadorSimple.obtenerNombre());
+	}
+	
+	@Test
+	public void testAlmacenarCorreoDemasiadoGrande() {
+		
+		String nombreArchivadorSimple = "Archivador Simple";
+		int espacio = 5;
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
+		
+		Texto texto = new Texto("Nombre Texto", "Contenido Texto");
+		
+		Correo correo = new Mensaje(texto);;
+		
+		assertEquals(false, archivadorSimple.almacenarCorreo(correo));
 	}
 	
 	@Test
@@ -26,13 +40,15 @@ public class ArchivadorSimpleTest {
 		
 		String nombreArchivadorSimple = "Archivador Simple";
 		int espacio = 25;
-		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
 		
 		Texto texto = new Texto("Nombre Texto", "Contenido Texto");
 		
 		Correo correo = new Mensaje(texto);
 		
 		assertEquals(true, archivadorSimple.almacenarCorreo(correo));
+		
+		assertEquals(espacio, archivadorSimple.obtenerEspacioTotal());
 		
 		assertEquals(espacio - texto.obtenerTamaño(), archivadorSimple.obtenerEspacioDisponible());
 	}
@@ -42,7 +58,7 @@ public class ArchivadorSimpleTest {
 		String nombreArchivadorSimple = "Archivador Simple";
 		int espacio = 10;
 		
-		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
 		
 		assertEquals(espacio, archivadorSimple.obtenerEspacioTotal());
 	}
@@ -52,8 +68,19 @@ public class ArchivadorSimpleTest {
 		String nombreArchivadorSimple = "Archivador Simple";
 		int espacio = 10;
 		
-		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
 		
 		assertEquals(espacio, archivadorSimple.obtenerEspacioDisponible());
+	}
+	
+	@Test
+	public void testObtenerDelegado() {
+		String nombreArchivadorSimple = "Archivador Simple";
+		int espacio = 25;
+		Archivador archivador = new ArchivadorSimple(nombreArchivadorSimple, espacio);
+		
+		archivador.establecerDelegado(archivador);
+		
+		assertEquals(null, archivador.obtenerDelegado());
 	}
 }
