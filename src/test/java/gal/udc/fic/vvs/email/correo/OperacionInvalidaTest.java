86fcac35840f8c94abec80358b7127d2cf1a2d2e
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-01 12:07:40 +0100

    tests manuales de unidad entidad OperacionInvalida

---
+++ src/test/java/gal/udc/fic/vvs/email/correo/OperacionInvalidaTest.java
@@ -0,0 +1,20 @@
+package gal.udc.fic.vvs.email.correo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OperacionInvalidaTest {
+
+	/**
+	 * Probamos que podemos crear una excepcion OperacionInvalida dandole un String
+	 * donde podemos especificar un mensaje y obtenerlo
+	 */
+	@Test
+	public void test() {
+		
+		Exception e = new OperacionInvalida("Descripcion Operacion Invalida");
+		
+		assertEquals("Descripcion Operacion Invalida", e.getMessage());
+	}
+}
