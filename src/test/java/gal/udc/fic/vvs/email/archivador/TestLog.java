ac7f084fffa11d6b595e0e72f0067dbcd48746f2
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-03 14:14:02 +0100

    tests añadidos para Log

--- src/test/java/gal/udc/fic/vvs/email/archivador/TestLog.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/TestLog.java
@@ -11,7 +11,7 @@ import gal.udc.fic.vvs.email.correo.Mensaje;
 public class TestLog {
 
 	@Test
-	public void testLog() {
+	public void testLogAlmacenarCorreo() {
 		
 		String nombreArchivador = "Archivador";
 		int espacio = 25;
@@ -25,9 +25,61 @@ public class TestLog {
 		Correo correo = new Mensaje(texto);
 		
 		assertEquals(true, log.almacenarCorreo(correo));
+	}
+	
+	@Test
+	public void testLogAlmacenarCorreoYObtenerEspacioDisponible() {
+		
+		String nombreArchivador = "Archivador";
+		int espacio = 25;
+		
+		Archivador archivador = new ArchivadorSimple(nombreArchivador, espacio);
+		Delegado delegado = new Delegado(archivador);
+		
+		Log log = new Log(delegado);
+		
+		Texto texto = new Texto("Nombre Texto", "Contenido Texto");		
+		Correo correo = new Mensaje(texto);
+		
+		log.almacenarCorreo(correo);
 		
 		assertEquals(espacio - texto.obtenerTamaño(), log.obtenerEspacioDisponible());
+	}
+	
+	@Test
+	public void testLogObtenerNombre() {
+		
+		String nombreArchivador = "Archivador";
+		int espacio = 25;
+		
+		Archivador archivador = new ArchivadorSimple(nombreArchivador, espacio);
+		Delegado delegado = new Delegado(archivador);
+		
+		Log log = new Log(delegado);
+		
+		Texto texto = new Texto("Nombre Texto", "Contenido Texto");		
+		Correo correo = new Mensaje(texto);
+		
+		log.almacenarCorreo(correo);
+		
 		assertEquals(nombreArchivador, log.obtenerNombre());
+	}
+	
+	@Test
+	public void testLogEstablecerDelegadoObtenerDelegado() {
+		
+		String nombreArchivador = "Archivador";
+		int espacio = 25;
+		
+		Archivador archivador = new ArchivadorSimple(nombreArchivador, espacio);
+		Delegado delegado = new Delegado(archivador);
+		
+		Log log = new Log(delegado);
+		
+		Texto texto = new Texto("Nombre Texto", "Contenido Texto");		
+		Correo correo = new Mensaje(texto);
+		
+		log.almacenarCorreo(correo);
 	
 		log.establecerDelegado(delegado);
 		
