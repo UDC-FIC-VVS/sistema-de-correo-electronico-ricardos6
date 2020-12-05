f2bef82f00dc40a7590c483706883eee61e43271
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-05 22:58:00 +0100

    PBT de Audio

--- src/test/java/gal/udc/fic/vvs/email/archivo/TestAudio.java
+++ src/test/java/gal/udc/fic/vvs/email/archivo/TestAudio.java
@@ -1,49 +1,9 @@
 package gal.udc.fic.vvs.email.archivo;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
 public class TestAudio {
-
-	@Test
-	public void testObetenerNombre() {
-
-		String nombreAudio = "Nombre Audio";
-		String contenido = "Contenido Audio";
-		Audio audio = new Audio(nombreAudio, contenido);
-
-		assertEquals(nombreAudio, audio.obtenerNombre());
-	}
-
-	@Test
-	public void testObtenerContenido() {
-
-		String nombreAudio = "Nombre Audio";
-		String contenidoAudio = "Contenido Audio";
-		Audio audio = new Audio(nombreAudio, contenidoAudio);
-
-		assertEquals(contenidoAudio, audio.obtenerContenido());
-	}
-
-	@Test
-	public void testObtenerTamaño() {
-
-		String nombreAudio = "Nombre Audio";
-		String contenido = "Contenido Audio";
-		Audio audio = new Audio(nombreAudio, contenido);
-
-		assertEquals(contenido.length(), audio.obtenerTamaño());
-	}
-
-	@Test
-	public void testObtenerPreVisualizacion() {
-
-		String nombreAudio = "Nombre Audio";
-		String contenido = "Contenido Audio";
-		Audio audio = new Audio(nombreAudio, contenido);
-
-		assertEquals(nombreAudio + "(" + audio.obtenerTamaño() + " bytes, " + audio.obtenerMimeType() + ")",
-				audio.obtenerPreVisualizacion());
-	}
+	
 }
