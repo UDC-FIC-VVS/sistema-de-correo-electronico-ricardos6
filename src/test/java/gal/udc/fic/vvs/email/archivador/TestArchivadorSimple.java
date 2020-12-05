d38c68b167b2301503d3df35a2219a73affc5c45
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-05 22:59:16 +0100

    PBT de ArchivadorSimple

--- src/test/java/gal/udc/fic/vvs/email/archivador/TestArchivadorSimple.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/TestArchivadorSimple.java
@@ -8,79 +8,34 @@ import gal.udc.fic.vvs.email.archivo.Texto;
 import gal.udc.fic.vvs.email.correo.Correo;
 import gal.udc.fic.vvs.email.correo.Mensaje;
 
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
 public class TestArchivadorSimple {
 	
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Corner case donde el espacio del archivadorSimple es negativo		
+	 * 
+	 */
 	@Test
-	public void testObtenerNombre() {
+	public void testAlmacenarCorreoEspacioNegativo() {
 		
 		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 10;
-		
-		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
-		
-		assertEquals(nombreArchivadorSimple, archivadorSimple.obtenerNombre());
-	}
-	
-	@Test
-	public void testAlmacenarCorreoDemasiadoGrande() {
-		
-		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 5;
-		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
-		
-		Texto texto = new Texto("Nombre Texto", "Contenido Texto");
-		
-		Correo correo = new Mensaje(texto);;
-		
-		assertEquals(false, archivadorSimple.almacenarCorreo(correo));
-	}
-	
-	@Test
-	public void testAlmacenarCorreo() {
-		
-		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 25;
+		int espacio = -10;
 		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
 		
 		Texto texto = new Texto("Nombre Texto", "Contenido Texto");
 		
 		Correo correo = new Mensaje(texto);
 		
-		assertEquals(true, archivadorSimple.almacenarCorreo(correo));
-		
-		assertEquals(espacio, archivadorSimple.obtenerEspacioTotal());
-		
-		assertEquals(espacio - texto.obtenerTamaño(), archivadorSimple.obtenerEspacioDisponible());
-	}
-	
-	@Test
-	public void testObtenerEspacioTotal() {
-		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 10;
-		
-		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
-		
-		assertEquals(espacio, archivadorSimple.obtenerEspacioTotal());
-	}
-	
-	@Test
-	public void testObtenerEspacioDisponible() {
-		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 10;
-		
-		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
-		
-		assertEquals(espacio, archivadorSimple.obtenerEspacioDisponible());
-	}
-	
-	@Test
-	public void testObtenerDelegado() {
-		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 25;
-		Archivador archivador = new ArchivadorSimple(nombreArchivadorSimple, espacio);
-		
-		archivador.establecerDelegado(archivador);
-		
-		assertEquals(null, archivador.obtenerDelegado());
+		assertEquals(false, archivadorSimple.almacenarCorreo(correo));
 	}
 }
---
+++ src/test/java/gal/udc/fic/vvs/email/archivador/TestPropertyArchivadorSimple.java
@@ -0,0 +1,160 @@
+package gal.udc.fic.vvs.email.archivador;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeThat;
+
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import gal.udc.fic.vvs.email.archivo.Texto;
+import gal.udc.fic.vvs.email.correo.Correo;
+import gal.udc.fic.vvs.email.correo.Mensaje;
+
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
+@RunWith(JUnitQuickcheck.class)
+public class TestPropertyArchivadorSimple {
+
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Numeros enteros positivos			
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerNombre(String nombre, int espacio) {
+
+		assumeThat(espacio, greaterThan(-1));
+
+		Archivador archivadorSimple = new ArchivadorSimple(nombre, espacio);
+
+		assertEquals(nombre, archivadorSimple.obtenerNombre());
+	}
+	
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Numeros enteros positivos			
+	 * 
+	 */
+	@Property
+	public void testPropertyAlmacenarCorreo(String nombreArchivadorSimple, int espacioArchivadorSimple,
+			String nombreTexto, String contenidoTexto) {
+
+		assumeThat(espacioArchivadorSimple, greaterThan(-1));
+		assumeThat(espacioArchivadorSimple, greaterThan(contenidoTexto.length()));
+
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacioArchivadorSimple);
+
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+
+		Correo correo = new Mensaje(texto);
+
+		assertEquals(true, archivadorSimple.almacenarCorreo(correo));
+	}
+	
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Numeros enteros positivos			
+	 * 
+	 */
+	@Property
+	public void testPropertyAlmacenarCorreoDemaisadoGrande(String nombreArchivadorSimple, int espacioArchivadorSimple,
+			String nombreTexto, String contenidoTexto) {
+
+		assumeThat(espacioArchivadorSimple, greaterThan(-1));
+		assumeThat(contenidoTexto.length(), greaterThan(espacioArchivadorSimple));
+
+		Archivador archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacioArchivadorSimple);
+
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+
+		Correo correo = new Mensaje(texto);
+
+		assertEquals(false, archivadorSimple.almacenarCorreo(correo));
+	}
+
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Numeros enteros positivos generados automaticamente		
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerEspacioDisponible(String nombre, int espacio) {
+
+		Archivador archivadorSimple = new ArchivadorSimple(nombre, espacio);
+
+		assertEquals(espacio, archivadorSimple.obtenerEspacioDisponible());
+	}
+	
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Numeros enteros positivos generados automaticamente		
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerEspacioTotal(String nombre, int espacio) {
+
+		assumeThat(espacio, greaterThan(-1));
+		
+		Archivador archivadorSimple = new ArchivadorSimple(nombre, espacio);
+		
+		assertEquals(espacio, archivadorSimple.obtenerEspacioTotal());
+	}
+	
+	/**
+	 * Nivel da prueba: Unitaria
+	 * Categorías a las que pertenece: Funcionales
+	 * dinámicas de caja negra, positivas
+	 * Mecanismo de selección de los datos:
+	 * 		Categoria:
+	 * 			Strings cualesquiera. Como representate elegimos dos String
+	 * 			generados automaticamente.
+	 * 			Numeros enteros positivos generados automaticamente		
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerDelegado(String nombre, int espacio) {
+		
+		assumeThat(espacio, greaterThan(-1));
+		
+		Archivador archivador = new ArchivadorSimple(nombre, espacio);
+		
+		archivador.establecerDelegado(archivador);
+		
+		assertEquals(null, archivador.obtenerDelegado());
+	}
+}
