33293fbf6de712b9b90c58307669b417fe1602ec
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-05 22:57:35 +0100

    PBT de Texto

---
+++ src/test/java/gal/udc/fic/vvs/email/archivo/TestPropertyTexto.java
@@ -0,0 +1,82 @@
+package gal.udc.fic.vvs.email.archivo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+/**
+ * 
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
+@RunWith(JUnitQuickcheck.class)
+public class TestPropertyTexto {
+
+	/**
+	 * - Nivel da prueba: Unitaria
+	 * - Categorías a las que pertenece: Funcionales dinámicas de caja negra, positivas
+	 * - Mecanismo de selección de los datos: 
+	 * 		Categoria: Strings cualesquiera. Como representate elegimos dos String
+	 * 		generados automaticamente.
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerNombre(String nombre, String contenido) {
+		
+		Archivo archivo = new Texto(nombre, contenido);
+	
+		assertEquals(nombre, archivo.obtenerNombre());
+	}
+	
+	/**
+	 * - Nivel da prueba: Unitaria
+	 * - Categorías a las que pertenece: Funcionales dinámicas de caja negra, positivas
+	 * - Mecanismo de selección de los datos: 
+	 * 		Categoria: Strings cualesquiera. Como representate elegimos dos String
+	 * 		generados automaticamente.
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerContenido(String nombre, String contenido) {
+		
+		Archivo archivo = new Texto(nombre, contenido);
+	
+		assertEquals(contenido, archivo.obtenerContenido());
+	}
+	
+	/**
+	 * - Nivel da prueba: Unitaria
+	 * - Categorías a las que pertenece: Funcionales dinámicas de caja negra, positivas
+	 * - Mecanismo de selección de los datos: 
+	 * 		Categoria: Strings cualesquiera. Como representate elegimos dos String
+	 * 		generados automaticamente.
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerTamaño(String nombre, String contenido) {
+		
+		Archivo archivo = new Texto(nombre, contenido);
+	
+		assertEquals(contenido, archivo.obtenerContenido());
+	}
+	
+	/**
+	 * - Nivel da prueba: Unitaria
+	 * - Categorías a las que pertenece: Funcionales dinámicas de caja negra, positivas
+	 * - Mecanismo de selección de los datos: 
+	 * 		Categoria: Strings cualesquiera. Como representate elegimos dos String
+	 * 		generados automaticamente.
+	 * 
+	 */
+	@Property
+	public void testPropertyObtenerPreVisualizacion(String nombre, String contenido) {
+
+		Archivo archivo = new Texto(nombre, contenido);
+
+		assertEquals(nombre + "(" + archivo.obtenerTamaño() + " bytes, " + archivo.obtenerMimeType() + ")",
+				archivo.obtenerPreVisualizacion());
+	}
+}
--- src/test/java/gal/udc/fic/vvs/email/archivo/TestTexto.java
+++ src/test/java/gal/udc/fic/vvs/email/archivo/TestTexto.java
@@ -1,48 +1,9 @@
 package gal.udc.fic.vvs.email.archivo;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
 public class TestTexto {
 	
-	@Test
-	 public void testObtenerNombre() {
-		 
-		 String nombreTexto = "Nombre Texto";
-		 String contenido = "Contenido Texto";
-		 Texto texto = new Texto(nombreTexto, contenido);
-		 
-		 assertEquals(nombreTexto, texto.obtenerNombre());
-	 }
-	 
-	 @Test
-	 public void testObtenerContenido() {
-		 
-		 String nombreTexto = "Nombre Texto";
-		 String contenidoTexto = "Contenido Texto";
-		 Texto texto = new Texto(nombreTexto, contenidoTexto);
-		 
-		 assertEquals(contenidoTexto, texto.obtenerContenido());
-	 }
-	 
-	 @Test
-	 public void testObtenerTamaño() {
-		 
-		 String nombreTexto = "Nombre Texto";
-		 String contenido = "Contenido Texto";
-		 Texto texto = new Texto(nombreTexto, contenido);
-		 
-		 assertEquals(contenido.length(), texto.obtenerTamaño());
-	 }
-	 
-	 @Test
-	 public void testObtenerPreVisualizacion() {
-		 
-		 String nombreTexto = "Nombre Texto";
-		 String contenido = "Contenido Texto";
-		 Texto texto = new Texto(nombreTexto, contenido);
-		 
-		 assertEquals(nombreTexto + "(" + texto.obtenerTamaño() + " bytes, " +  texto.obtenerMimeType() + ")", texto.obtenerPreVisualizacion());
-	 }
 }
