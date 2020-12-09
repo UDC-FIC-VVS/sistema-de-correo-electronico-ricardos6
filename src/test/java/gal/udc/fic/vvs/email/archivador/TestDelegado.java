9a4c98f50beec8883287058a5a4f1fdbf0693146
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-09 19:38:41 +0100

    PBT y mejora de javadoc para TestDelegado y TestPropertyDelegado

--- src/test/java/gal/udc/fic/vvs/email/archivador/TestDelegado.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/TestDelegado.java
@@ -8,38 +8,37 @@ import gal.udc.fic.vvs.email.archivo.Texto;
 import gal.udc.fic.vvs.email.correo.Correo;
 import gal.udc.fic.vvs.email.correo.Mensaje;
 
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
 public class TestDelegado {
-	
-	@Test	
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * negativas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * Corner case en el que el espacio del archivador es 0 y salta una excepcion al
+	 * intentar almacenar un correo con el delegado.
+	 */
+	@Test(expected = NullPointerException.class)
 	public void testAlmacenarCorreo() {
-		
-		String nombreArchivadorSimple = "Archivador Simple";
-		int espacio = 25;
-		
-		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombreArchivadorSimple, espacio);
-		
+
+		ArchivadorSimple archivadorSimple = new ArchivadorSimple("nombreArchivadorSimple", 0);
+
 		Delegado delegado = new Delegado(archivadorSimple);
-		
-		Texto texto = new Texto("Nombre Texto", "Contenido Texto");
-		
+
+		// creamos un correo de texto para insertar desde el delegado
+		Texto texto = new Texto("nombreTexto", "contenidoTexto");
+
 		Correo correo = new Mensaje(texto);
-		
-		assertEquals(true, delegado.almacenarCorreo(correo));
-	}
-	
-	@Test
-	public void testEstablecerYObtenerDelegado() {
-		
-		String nombreArchivador = "Archivador";
-		int espacio = 25;
-		
-		Archivador archivador = new ArchivadorSimple(nombreArchivador, espacio);
-		
-		Delegado delegado = new Delegado(archivador);
-		
-		delegado.establecerDelegado(archivador);
-		
-		assertEquals(archivador, delegado.obtenerDelegado());
-		
+
+		assertEquals(false, delegado.almacenarCorreo(correo));
 	}
 }
