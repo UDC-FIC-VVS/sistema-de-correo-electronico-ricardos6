a1e8f51136261fbee47082911dbe8a633a184cf6
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-15 19:53:13 +0100

    PBT de Carpeta

--- src/test/java/gal/udc/fic/vvs/email/correo/TestCarpeta.java
+++ src/test/java/gal/udc/fic/vvs/email/correo/TestCarpeta.java
@@ -1,5 +1,9 @@
 package gal.udc.fic.vvs.email.correo;
 
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -7,241 +11,39 @@ import org.junit.Test;
 import gal.udc.fic.vvs.email.archivo.Texto;
 
 public class TestCarpeta {
-	
+
 	/**
-	 * Añadimos un mensaje a la carpeta y comprobamos su tamaño
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
 	 * 
 	 * @throws OperacionInvalida
 	 */
 	@Test
-	public void testAñadir() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");
-		
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		
-		carpeta.añadir(new Mensaje(texto));
-				
-		assertEquals(texto.obtenerTamaño(), carpeta.obtenerTamaño());
-	}
-	
-	/**
-	 * Añadimos un mensaje que ya esta dentro de una carpeta en otra carpeta.
-	 * La primera carpeta se elimina como padre del mensaje y se establece la nueva
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testAñadirCarpetaHijo() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");
-		Carpeta carpeta2 = new Carpeta("Carpeta Padre");		
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		
+	public void testBuscar() throws OperacionInvalida {
+
+		String nombreCarpeta = "nombreCarpeta";
+		String nombreTexto = "nombreTexto";
+		String contenidoTexto = "contenidoTexto";
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
 		carpeta.añadir(mensaje);
-		carpeta2.añadir(mensaje);
-				
-		assertEquals(texto.obtenerTamaño(), carpeta2.obtenerTamaño());
-		assertEquals(carpeta2, mensaje.obtenerPadre());
-		assertEquals(mensaje, carpeta2.obtenerHijo(0));
-	}
-	
-	/**
-	 * Teniendo dos mensajes dentro de un carpeta establecemos como leido la carpeta
-	 * y se establecen como leido sus correos
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testEstablecerLeido() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		assertEquals(2, carpeta.obtenerNoLeidos());
-		
-		carpeta.establecerLeido(true);
-				
-		assertEquals(0, carpeta.obtenerNoLeidos());
-	}
-	
-	/**
-	 * Teniendo dos mensajes dentro de un carpeta establecemos como leido la carpeta
-	 * y se establecen como leido sus correos
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerNoLeido() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		assertEquals(2, carpeta.obtenerNoLeidos());
-	}
-	
-	/**
-	 * Teniendo dos mensajes dentro de un carpeta establecemos como leido la carpeta
-	 * y se establecen como leido sus correos
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testObtenerIcono() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		
-		assertEquals(new Integer(1), carpeta.obtenerIcono());
-	}
-	
-	/**
-	 * Obtenemos previsualizacion de una carpeta con 2 mensajes dentro
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerPreVisualizacion() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		assertEquals("Carpeta (2)", carpeta.obtenerPreVisualizacion());
-	}
-	
-	/**
-	 * Obtenemos visualizacion de una carpeta con 2 mensajes dentro
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerVisualizacion() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		assertEquals("Carpeta (2)", carpeta.obtenerVisualizacion());
-	}
-	
-	/**
-	 * Añadimos dos mensajes a la carpeta y los obtenemmos en una lista
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerHijos() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		assertEquals(true, carpeta.explorar().contains(mensaje));
-		assertEquals(true, carpeta.explorar().contains(mensaje2));
-	}
-	
-	/**
-	 * Obtenemos visualizacion de una carpeta con 2 mensajes dentro
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerBuscar() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		assertEquals(true, carpeta.buscar("o").contains(mensaje));
-		assertEquals(true, carpeta.buscar("o").contains(mensaje2));
-		assertEquals(2, carpeta.buscar("o").size());
-	}
-	
-	/**
-	 * Borramos uno de los mensajes que hay dentro de una carpeta
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testEliminar() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);
-		
-		carpeta.eliminar(mensaje2);
-		
-		assertEquals(true, carpeta.buscar("o").contains(mensaje));
-		assertEquals(1, carpeta.buscar("o").size());
-	}
-	
-	/**
-	 * Borramos uno de los mensajes que hay dentro de una carpeta
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerHijo() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");	
-		Texto texto = new Texto("contido", "Contido do correo electrónico.");
-		Mensaje mensaje = new Mensaje(texto);
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(mensaje);
-		carpeta.añadir(mensaje2);		
-		
-		assertEquals(mensaje, carpeta.obtenerHijo(0));
-		assertEquals(mensaje2, carpeta.obtenerHijo(1));
-	}
-	
-	/**
-	 * Borramos uno de los mensajes que hay dentro de una carpeta
-	 * 
-	 * @throws OperacionInvalida
-	 */
-	@Test
-	public void testObtenerRuta() throws OperacionInvalida {
-		
-		Carpeta carpeta = new Carpeta("Carpeta");
-		Carpeta carpeta2 = new Carpeta("Carpeta2");
-		Mensaje mensaje2 = new Mensaje(new Texto("Nombre 2", "Texto 2"));
-		
-		carpeta.añadir(carpeta2);
-		carpeta.añadir(mensaje2);		
-		
-		assertEquals("Carpeta (1) > Carpeta2", carpeta2.obtenerRuta());
+
+		assertEquals(true, carpeta.buscar(contenidoTexto).contains(mensaje));
 	}
 }
---
+++ src/test/java/gal/udc/fic/vvs/email/correo/TestPropertyCarpeta.java
@@ -0,0 +1,402 @@
+package gal.udc.fic.vvs.email.correo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import gal.udc.fic.vvs.email.archivo.Texto;
+
+/**
+ * @author r.sanchezarias6@gmail.com
+ *
+ */
+@RunWith(JUnitQuickcheck.class)
+public class TestPropertyCarpeta {
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyAñadir(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(mensaje.obtenerTamaño(), carpeta.obtenerTamaño());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta      Categoria: String cualquiera. Representantes:
+	 *                           Generados automaticamente.
+	 * @param nombreTexto        Categoria: String cualquiera. Representantes:
+	 *                           Generados automaticamente.
+	 * @param contenidoTexto     Categoria: String cualquiera. Representantes:
+	 *                           Generados automaticamente.
+	 * @param nombreCarpetaPadre Categoria: String cualquiera. Representantes:
+	 *                           Generados automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyAñadirCarpetaHijo(String nombreCarpeta, String nombreTexto, String contenidoTexto,
+			String nombreCarpetaPadre) throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Carpeta carpetaPadre = new Carpeta(nombreCarpetaPadre);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+		carpetaPadre.añadir(carpeta);
+
+		assertEquals(mensaje.obtenerTamaño(), carpetaPadre.obtenerTamaño());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyEstablecerLeido(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+		carpeta.establecerLeido(true);
+		;
+
+		assertEquals(0, carpeta.obtenerNoLeidos());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerNoLeido(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(1, carpeta.obtenerNoLeidos());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta Categoria: String cualquiera. Representantes: Generados
+	 *                      automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerIcono(String nombreCarpeta) throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+
+		assertEquals(Correo.ICONO_CARPETA, carpeta.obtenerIcono());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerPreVisualizacion(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(nombreCarpeta + " (1)", carpeta.obtenerPreVisualizacion());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerVisualizacion(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(nombreCarpeta + " (1)", carpeta.obtenerVisualizacion());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyExplorar(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(true, carpeta.explorar().contains(mensaje));
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyEliminar(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+		carpeta.eliminar(mensaje);
+
+		assertEquals(false, carpeta.explorar().contains(mensaje));
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerHijo(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(mensaje, carpeta.obtenerHijo(0));
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerRuta(String nombreCarpeta, String nombreTexto, String contenidoTexto)
+			throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+
+		assertEquals(nombreCarpeta + " (1)", carpeta.obtenerRuta());
+	}
+
+	/**
+	 * 
+	 * <ul>
+	 * <li>Nivel da prueba: Unitaria</li>
+	 * <li>Categorías a las que pertenece: Funcionales dinámicas de caja negra,
+	 * positivas</li>
+	 * </ul>
+	 * <p>
+	 * *
+	 * 
+	 * @param nombreCarpeta  Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param nombreTexto    Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * @param contenidoTexto Categoria: String cualquiera. Representantes: Generados
+	 *                       automaticamente.
+	 * 
+	 * @throws OperacionInvalida
+	 */
+	@Property
+	public void testPropertyObtenerRutaConCarpetaPadre(String nombreCarpeta, String nombreTexto, String contenidoTexto,
+			String nombreCarpetaPadre) throws OperacionInvalida {
+
+		Carpeta carpeta = new Carpeta(nombreCarpeta);
+		Carpeta carpetaPadre = new Carpeta(nombreCarpetaPadre);
+		Texto texto = new Texto(nombreTexto, contenidoTexto);
+		MensajeAbstracto mensaje = new Mensaje(texto);
+
+		carpeta.añadir(mensaje);
+		carpetaPadre.añadir(carpeta);
+
+		assertEquals(nombreCarpetaPadre + " (1) > " + nombreCarpeta + " (1)", carpeta.obtenerRuta());
+	}
+}
